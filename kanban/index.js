6c62ebfebb2f66c8a5da8fbac3800ac2eb0cbe3b
govind07 <govindrajput02653@gmail.com>
2025-03-05 16:44:30 +0530

    span tag added

--- kanban/index.js
+++ kanban/index.js
@@ -1,6 +1,27 @@
 const addTaskBtn = document.getElementById('add-task-btn')
 const todoBoard = document.getElementById('todo-board')
 
+
+
+function attachDragEvents(target){
+    target.addEventListener('dragstart',()=>{
+        target.classList.add('flying')
+    })
+    target.addEventListener('dragend',()=>{
+        target.classList.remove('flying')
+    })
+}
+
+function removebtn(target){
+    let btn = document.createElement('button');
+    btn.innerText = "X"
+    target.appendChild(btn);
+    btn.addEventListener("click", () => {
+        target.remove();
+    })
+
+}
+
 //add event listener on btn
 addTaskBtn.addEventListener('click',()=>{
     const input = prompt('what is the task?')
@@ -8,8 +29,15 @@ addTaskBtn.addEventListener('click',()=>{
 
     //creating p tag
     let p = document.createElement('p');
+    let btn = document.createElement('span');
+    btn.classList.add("del-btn")
+    btn.textContent = "XX"
+    p.appendChild(btn)
     p.classList.add("item");
     p.setAttribute('draggable',true)
+    p.contentEditable = true
+    attachDragEvents(p)
+    
     p.innerText = input;
     todoBoard.appendChild(p)
 
@@ -20,15 +48,7 @@ const allBoards = document.querySelectorAll('.board');
 const allitems = document.querySelectorAll('.item');
 
 
-allitems.forEach((item)=>{
-    item.addEventListener('dragstart',()=>{
-        item.classList.add('flying')
-    })
-    item.addEventListener('dragend',()=>{
-        item.classList.remove('flying')
-    })
-
-})
+allitems.forEach(attachDragEvents)
 
 
 
